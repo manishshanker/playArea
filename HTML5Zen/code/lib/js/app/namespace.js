02af0549ef1000b937beea8b56f07a9e759036b8
Manish Shanker <manishshanker@hotmail.com>
2013-12-10 16:12:03 +0000

    added application build module

--- HTML5Zen/code/lib/js/app/namespace.js
+++ HTML5Zen/code/lib/js/app/namespace.js
@@ -1,5 +1,10 @@
-window.APP = {};
-APP.controller = {};
-APP.service = {};
-APP.view = {};
-APP.template = {};
+(function(window){
+    "use strict";
+
+    window.APP = window.APP || {};
+    APP.controller = {};
+    APP.service = {};
+    APP.view = {};
+    APP.template = {};
+
+}(window));
