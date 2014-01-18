b2ff24a55677897a1f8a11d6be6a1e9dc3c52120
Manish Shanker <manishshanker@hotmail.com>
2014-01-18 14:39:18 +0000

    more improvements

--- HAF_Framework/src/js/baseClass/View.js
+++ HAF_Framework/src/js/baseClass/View.js
@@ -2,14 +2,24 @@
     "use strict";
 
     HAF.View = HAF.Base.extend({
+        autoBindManagement: false,
         init: function (dependencies) {
             this.injectDependencies(dependencies);
             this.$container = $(this.container);
             this.$el = this.$container.$item;
-            this.bind();
+            if (!this.autoBindManagement) {
+                this.bind();
+            }
         },
         container: null,
         $container: null,
+        bindings: null,
+        bind: function () {
+            bindEvents(this);
+        },
+        unbind: function () {
+            unbindEvents(this);
+        },
         $el: null,
         render: function (html) {
             this.$container.html(html);
@@ -17,13 +27,34 @@
         destroy: function () {
             this.$container.empty();
         },
-        bind: HAF.noop,
         hide: function () {
             this.$el.hide();
+            if (this.autoBindManagement) {
+                this.unbind();
+            }
         },
         show: function () {
             this.$el.show();
+            if (this.autoBindManagement) {
+                this.bind();
+            }
         }
     });
+
+    function unbindEvents(ctx) {
+        HAF.each(ctx.bindings, function (fn, key) {
+            var parts = /([a-z]+)\s([a-zA-Z0-9\-\.\(\)>]+)/.exec(key);
+            ctx.$container.off(parts[1], parts[2]);
+        });
+    }
+
+    function bindEvents(ctx) {
+        HAF.each(ctx.bindings, function (fn, key) {
+            var parts = /([a-z]+)\s([a-zA-Z0-9\-\.\(\)>]+)/.exec(key);
+            ctx.$container.on(parts[1], parts[2], function (e) {
+                fn.call(ctx, e, this);
+            });
+        });
+    }
 
 }(HAF, HAF.DOM));
