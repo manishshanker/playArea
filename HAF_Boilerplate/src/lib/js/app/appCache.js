9d20b5bae2a51e772a0d3389c75d5a135a42931f
Manish Shanker <manishshanker@hotmail.com>
2014-01-21 16:58:33 +0000

    fixed jshint errors

--- HAF_Boilerplate/src/lib/js/app/appCache.js
+++ HAF_Boilerplate/src/lib/js/app/appCache.js
@@ -4,17 +4,15 @@
 
         window.addEventListener('load', function () {
             window.applicationCache.addEventListener('updateready', function (e) {
-                if (window.applicationCache.status == window.applicationCache.UPDATEREADY) {
+                if (window.applicationCache.status === window.applicationCache.UPDATEREADY) {
                     // Browser downloaded a new app cache.
                     if (confirm('A new version of this application is available. Load it?')) {
                         window.location.reload();
                     }
-                } else {
-                    // Manifest didn't changed. Nothing new to server.
                 }
             }, false);
         }, false);
 
     }
-})(window);
+}(window));
 
