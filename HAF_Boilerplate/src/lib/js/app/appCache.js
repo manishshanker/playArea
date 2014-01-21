7513ea0d0f322d1b290234bf56c0f91b92c40338
Manish Shanker <manishshanker@hotmail.com>
2014-01-21 17:11:24 +0000

    fixed jshint errors

--- HAF_Boilerplate/src/lib/js/app/appCache.js
+++ HAF_Boilerplate/src/lib/js/app/appCache.js
@@ -1,9 +1,12 @@
 (function (window) {
     "use strict";
+    //noinspection JSUnresolvedVariable
     if (window.addEventListener && window.applicationCache) {
 
         window.addEventListener('load', function () {
-            window.applicationCache.addEventListener('updateready', function (e) {
+            //noinspection JSUnresolvedVariable
+            window.applicationCache.addEventListener('updateready', function () {
+                //noinspection JSUnresolvedVariable
                 if (window.applicationCache.status === window.applicationCache.UPDATEREADY) {
                     // Browser downloaded a new app cache.
                     if (confirm('A new version of this application is available. Load it?')) {
