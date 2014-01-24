0cd8971978eedc8d91a527b344ae0525828b7d57
Manish Shanker <manishshanker@hotmail.com>
2014-01-24 16:21:13 +0000

    improvements

--- HAF_Framework/src/js/messaging.js
+++ HAF_Framework/src/js/messaging.js
@@ -9,17 +9,36 @@
         publish: function (subject, message) {
             this.messageBus.trigger(subject, [message]);
         },
-        subscribe: function (scope, subject, callback) {
-            var unsubscribeMethod = function (e, message) {
-                callback.call(scope, message);
-            };
-            this.messageBus.on(subject, unsubscribeMethod);
-            return unsubscribeMethod;
+        subscribe: function (scope, subjects, fn) {
+            var that = this;
+            if (typeof subjects === "string") {
+                return getSubsricber(that, fn, scope, subjects);
+            }
+            var subscriberFNs = {};
+            HAF.each(subjects, function (fn, subject) {
+                subscriberFNs[subject] = getSubsricber(that, fn, scope, subject);
+            });
+            return subscriberFNs;
         },
-        unsubscribe: function (subject, fn) {
-            this.messageBus.off(subject, fn);
+        unsubscribe: function (subjects, fn) {
+            var that = this;
+            if (typeof subjects === "string") {
+                that.messageBus.off(subjects, fn);
+            } else {
+                HAF.each(subjects, function (fn, subject) {
+                    that.messageBus.off(subject, fn);
+                });
+            }
         }
     };
+
+    function getSubsricber(ctx, fn, scope, subject) {
+        var unsubscribeMethod = function (e, message) {
+            fn.call(scope, message);
+        };
+        ctx.messageBus.on(subject, unsubscribeMethod);
+        return unsubscribeMethod;
+    }
 
     HAF.messaging = new Messaging();
     HAF.Messaging = Messaging;
